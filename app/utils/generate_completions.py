264567be24056d6d52d09d36fb6ab0146b448367
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-06-24 12:16:08 +0300

    exclude 3rd party dependencies from autocompletions

--- app/utils/generate_completions.py
+++ app/utils/generate_completions.py
@@ -1,6 +1,61 @@
 import jedi
 
-exclusions = {"fastapi"}
+exclusions = {
+    "annotated-types",
+    "anyio",
+    "astor",
+    "attrs",
+    "cattrs",
+    "certifi",
+    "click",
+    "dnspython",
+    "docstring-to-markdown",
+    "email_validator",
+    "exceptiongroup",
+    "fastapi",
+    "fastapi-cli",
+    "graphviz",
+    "h11",
+    "httpcore",
+    "httptools",
+    "httpx",
+    "idna",
+    "jedi",
+    "jedi-language-server",
+    "Jinja2",
+    "lsprotocol",
+    "markdown-it-py",
+    "MarkupSafe",
+    "mdurl",
+    "orjson",
+    "parso",
+    "pip",
+    "pluggy",
+    "pydantic",
+    "pydantic_core",
+    "pygls",
+    "Pygments",
+    "python-dotenv",
+    "python-jsonrpc-server",
+    "python-language-server",
+    "python-multipart",
+    "PyYAML",
+    "RestrictedPython",
+    "rich",
+    "setuptools",
+    "shellingham",
+    "sniffio",
+    "starlette",
+    "typed-ast",
+    "typer",
+    "typing_extensions",
+    "ujson",
+    "uvicorn",
+    "uvloop",
+    "watchfiles",
+    "websockets",
+    "wheel",
+}
 
 
 def get_completions(src: str, line: int, character: int):
@@ -11,7 +66,11 @@ def get_completions(src: str, line: int, character: int):
         "id": 1,
         "result": {
             "completions": [
-                {"label": completion.name, "insertText": completion.name, "kind": completion.type}
+                {
+                    "label": completion.name,
+                    "insertText": completion.name,
+                    "kind": completion.type,
+                }
                 for completion in completions
                 if completion.name not in exclusions
             ]
