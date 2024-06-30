486e12d02258f4bea976c507714c95647f4c4ae8
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-06-30 15:17:27 +0300

    Refactor Diagram to CustomDiagram in AST transformation

--- app/utils/ast_utils.py
+++ app/utils/ast_utils.py
@@ -1,36 +1,19 @@
 import ast
+
 import astor
 
 
-class WithStatementVisitor(ast.NodeVisitor):
-    def __init__(self) -> None:
-        super().__init__()
-        self.args = []
-        self.kwargs = {}
+class DiagramTransformer(ast.NodeTransformer):
+    """Changes the Diagram class to CustomDiagram to prevent unnecessary writes to disk"""
 
-    def visit_With(self, node):
-        for item in node.items:
-            if (
-                isinstance(item.context_expr, ast.Call)
-                and item.context_expr.func.id == "Diagram"
-            ):
-                self.modify_args(item.context_expr)
-
-        self.generic_visit(node)
-
-    def modify_args(self, node):
-        # new_arg = ast.Constant(value="file")
-        # node.args = [new_arg]
-        kwargs = [
-            ast.keyword(arg="filename", value=ast.Constant(value="static/file")),
-            ast.keyword(arg="outformat", value=ast.Constant(value="svg")),
-            ast.keyword(arg="show", value=ast.Constant(value=False)),
-        ]
-        node.keywords = kwargs
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Name) and node.func.id == "Diagram":
+            node.func.id = "CustomDiagram"
+        return node
 
 
-def modify_diagram_args(source_code: str) -> str:
+def modify_diagram(source_code: ast.Module) -> str:
     tree = ast.parse(source_code)
-    visitor = WithStatementVisitor()
-    visitor.visit(tree)
-    return astor.to_source(tree)
+    transformer = DiagramTransformer()
+    transformed_tree = transformer.visit(tree)
+    return astor.to_source(transformed_tree)
